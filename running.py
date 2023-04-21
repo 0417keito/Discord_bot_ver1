6628f20745a1c5df89b6f98ab896cbabefd4d449
0417keito <117526884+0417keito@users.noreply.github.com>
2023-04-22 03:17:38 +0900

    Update running.py

--- running.py
+++ running.py
@@ -2,13 +2,27 @@ import sys
 from convert_utils import whisper, send_chatgpt, convert_audio, infer
 
 
-def run(audio_path):
-     message = whisper(audio_path)
+
+def run(wav_file):
+     '''
+     with open(pcm_file, 'rb') as pcm_f:
+        pcm_data = pcm_f.read()
+        time = datetime.datetime.now()
+        wav_file = '/audio_file/discord_stream_audio/wav_file/output_{}.wav'.format(time)
+        with wave.open(wav_file, 'wb') as wav_f:
+            wav_f.setparams((channels, sample_width, sample_rate, 0, 'NONE', 'NONE'))
+            wav_f.writeframes(pcm_data)
+     '''
+     
+            
+     message = whisper(wav_file)
      result = send_chatgpt(message)
      reply_audio = convert_audio(result)
-     infer(input_audio=reply_audio)
+     output = infer(input_audio=reply_audio)
+     print(output)
      
      
-     
-audio_path = sys.argv[1]
-run(audio_path=audio_path)
+if __name__ == '__main__':
+     audio_path = sys.argv[1]
+     run(wav_file=audio_path)
+
