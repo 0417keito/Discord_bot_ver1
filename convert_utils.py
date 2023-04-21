a3e7e72da5c7874fd2e392033ee9e213c5f550ac
0417keito <117526884+0417keito@users.noreply.github.com>
2023-04-22 03:16:05 +0900

    Update convert_utils.py

--- convert_utils.py
+++ convert_utils.py
@@ -1,80 +1,174 @@
-import torch
-import numpy as np
-from scipy.io.wavfile import write
-from modules.models import VC_MODEL
-import os
-from modules.cmd_opts import opts
-from modules.shared import ROOT_DIR
-MODELS_DIR = opts.models_dir or os.path.join(ROOT_DIR, "models")
-
-from faster_whisper import WhisperModel
-
-from gtts import gTTS
-
-model_name = "kato_conversation.pth"
-
-OPENAI_KEY = 'your openai key'
-
-import openai
-openai.api_key = OPENAI_KEY
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-spk_item = 0
-vc_transform0 = 0
-input_audio0 = "C:\\Users\\mnooh\\Downloads\\Shorts.mp3"
-f0method0 = 'harvest'
-file_index1 =  "weights/kato_conversation/added_IVF657_Flat_nprobe_7"
-file_big_npy1 = "weights/kato_conversation/total_fea.npy"
-index_rate1 = 1
-f0_file = None
-
-#OpenAIのwhisperのAPIにファイルをおくってそれを文字お越しする
-def whisper(audio_path):
-    audio_file = open(audio_path, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print(transcript["text"])
+const fs = require("fs");
+const path = require("path");
+const { Readable, PassThrough } = require("stream");
+const { Client, GatewayIntentBits } = require("discord.js");
+const ytdl = require("ytdl-core");
+const wav = require("wav");
 
 
-'''
-def whisper(audio_path):
-    model_size = "large-v2"
-    model = WhisperModel(model_size, device="cuda", compute_type="float16")
-    segments, info = model.transcribe(audio=audio_path, beam_size=5)
-    messages = []
-    for segment in segments:
-        text = segment.text
-        messages.append(text)
-    return messages
-'''            
-       
- 
-def send_chatgpt(message):
-    res = openai.ChatCompletion.create(
-        model = 'gpt-3.5-turbo',
-        message = [{'role':'user', 'content':message}])
-    
-    return(res['choices'][0]['message']['content'])
+const PCMVolume = require("pcm-volume");
 
+const { runPythonScript } = require("./run_python");
+const { play_audio } = require("./voice-commands");
+const { end_of_stream } = require("./end_of_stream");
 
-def convert_audio(result):
-    tts = gTTS(result, lang='ja') 
-    tts.save('audio_file/TTS_audio/gTTS_test.mp3')
-    reply_audio = 'audio_file/TTS_audio/gTTS_test.mp3'
-    return reply_audio
+const Discord = require("discord.js");
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  createDiscordJSAdapter,
+  EndBehaviorType,
+  NoSubscriberBehavior,
+  StreamType,
+} = require("@discordjs/voice");
+const opus = require("@discordjs/opus");
+const Prism = require("prism-media");
+const AudioMixer = require("audio-mixer");
 
-def infer(sid = spk_item,input_audio = input_audio0,f0_up_key = vc_transform0,f0_file = f0_file,
-        f0_method = f0method0,file_index = file_index1,file_big_npy = file_big_npy1,index_rate = index_rate1,):
-    
-        model_path = os.path.join(MODELS_DIR, "checkpoints", model_name)
-        weight = torch.load(model_path, map_location="cpu")
-        model = VC_MODEL(model_name, weight)
-        audio = model.single(sid,input_audio,f0_up_key,f0_file,f0_method,file_index,file_big_npy,index_rate,)
+const client = new Client({
+  intents: Object.values(GatewayIntentBits).filter(Number.isInteger), // ALL Intents
+});
 
-        tgt_sr = model.tgt_sr
-        audio_opt = audio
-    
-        output_file = 'audio_file/output_audio/output.wav'
-        write(output_file, tgt_sr, audio_opt.astype(np.int16))
-        return output_file
-        
+class Silence extends Readable {
+  _read() {
+    this.push(Buffer.from([0xF8, 0xFF, 0xFE]));
+  }
+}
 
+const config = JSON.parse(fs.readFileSync("config.json"));
+
+client.on("ready", () => {
+  console.log(`Up and running.`);
+});
+
+const silenceTimeouts = {};
+
+function handleSilenceTimeout(userId,player,time,) {
+  console.log("Silence duration reached. Stopping recording.");
+  clearTimeout(silenceTimeouts[userId]);
+  silenceTimeouts[userId] = null;
+  processAudio(userId, player, time);
+}
+
+client.on("messageCreate", async (ctx) => {
+  if (!ctx.content.startsWith(config.prefix)) return;
+
+  const command = ctx.content.slice(config.prefix.length).split(" ");
+
+  switch (command[0]) {
+    case "join":
+      console.log("join");
+      client.on("debug", console.log);
+      if (ctx.member.voice.channelId) {
+        const channel = await client.channels.fetch(ctx.member.voice.channelId);
+
+        const connection = await joinVoiceChannel({
+          channelId: channel.id,
+          guildId: channel.guild.id,
+          adapterCreator: channel.guild.voiceAdapterCreator,
+          selfMute: false,
+          selfDeaf: false,
+        });
+
+        console.log("connection");
+
+        const player = createAudioPlayer({
+          behaviors: { noSubscriber: NoSubscriberBehavior.Play },
+        });
+        connection.subscribe(player);
+
+        const silenceResource = createAudioResource(new Silence(), { inputType: StreamType.Opus });
+        player.play(silenceResource);
+
+        console.log("y");
+
+        ctx.channel.send("I'm listening.. My hotword is **bumblebee**.");
+        console.log("x");
+
+        connection.receiver.speaking.on("start", (userId) => {
+          console.log("User started speaking: ", userId);
+          if (silenceTimeouts[userId]) {
+            clearTimeout(silenceTimeouts[userId]);
+            silenceTimeouts[userId] = null;
+          }
+
+          const audio = connection.receiver.subscribe(userId, {
+            end: { behavior: EndBehaviorType.AfterSilence, duration: 5000 },
+          });
+          const rawStream = new PassThrough();
+          const time = Date.now();
+          const filepath = `./audio_file/discord_stream_audio/pcm_file/user_audio_${userId}_${time}.pcm`;
+          ensureDirectoryExists(filepath);
+          const fileStream = fs.createWriteStream(filepath);
+
+          audio.pipe(new Prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 })).pipe(rawStream).pipe(fileStream);
+
+          connection.receiver.speaking.on("end", (userId) => {
+            audio.unpipe(rawStream);
+            rawStream.unpipe(fileStream);    
+            fileStream.end();
+            console.log("User stopped speaking: ", userId);
+            silenceTimeouts[userId] = setTimeout(
+              () => handleSilenceTimeout(userId,player,time),
+              5000 // 無音期間の duration (ミリ秒)
+            );
+            console.log("over");
+          });
+        });
+      }
+      break;
+  }
+});
+client.login(config.token);
+
+  
+
+async function processAudio(userId,player,time) {
+  try {
+    console.log("Stream ended.");
+
+    const pcmFilePath = path.resolve(`./audio_file/discord_stream_audio/pcm_file/user_audio_${userId}_${time}.pcm`);
+    const wavFilePath = path.resolve(`./audio_file/discord_stream_audio/wav_file/user_audio_${userId}_${time}.wav`);
+
+    const writer = new wav.FileWriter(wavFilePath, {
+      sampleRate: 48000,
+      channels: 2,
+    });
+
+    const fileStream = fs.createReadStream(pcmFilePath);
+    fileStream.pipe(writer);
+
+    await new Promise((resolve) => writer.on("finish", resolve));
+
+    await runPythonScript(wavFilePath).then((result) => {
+      console.log("Received output file path:", result);
+      const audioFileStream = fs.createReadStream(result);
+      const audioResource = createAudioResource(audioFileStream, {
+        inputType: StreamType.Arbitrary,
+      });
+      player.play(audioResource);
+
+      player.on("finish", () => {
+        player.stop();
+        console.log("finish");
+      });
+    }).catch((error) => {
+      console.log("Error occurred:", error);
+    });
+
+    player.on("finish", () => {
+      player.stop();
+      console.log("finish");
+    });
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
+
+function ensureDirectoryExists(filepath) {
+    const dir = path.dirname(filepath);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+  }
